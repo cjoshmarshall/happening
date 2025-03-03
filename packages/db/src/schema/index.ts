cbddaa78f2a3629cd5fad54e0d624feda3571b7e
cjoshmarshall <cjoshmarshall@gmail.com>
2025-03-03 10:21:47 +0530

    schema added

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,43 +1,84 @@
-// import { relations } from "drizzle-orm";
-import { date, integer, pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  date,
+  integer,
+  pgTable,
+  serial,
+  time,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const events = pgTable("Event", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }).notNull(),
-  duration: varchar("eventTime", { length: 256 }).notNull(),
+  duration: integer("duration").notNull(),
   ageRestriction: varchar("ageRestriction", { length: 256 }).notNull(),
-  eventDate: date("eventDate"),
+  eventDate: date("eventDate").notNull(),
   featuredImage: varchar("featuredImage", { length: 256 }).notNull(),
   additionalImage: varchar("additionalImage", { length: 256 }).notNull(),
 });
+
+export const eventRelations = relations(events, ({ many }) => ({
+  genres: many(genres),
+  languages: many(languages),
+  venues: many(venues),
+}));
 
 export const genres = pgTable("Genre", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }).notNull(),
 });
+
 export const languages = pgTable("Language", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }).notNull(),
 });
 
-export const eventClasses = pgTable("Event Class", {
+export const locations = pgTable("Location", {
   id: serial("id").primaryKey(),
-  name: varchar("name", { length: 256 }).notNull(),
-  noOfSeats: integer("noOfSeats").notNull(),
-  price: integer("price").notNull(),
+  street: varchar("name", { length: 256 }).notNull(),
+  city: varchar("city", { length: 256 }).notNull(),
 });
 
-export const locations = pgTable("Location", {
+export const venues = pgTable("Venue", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }).notNull(),
   city: varchar("city", { length: 256 }).notNull(),
 });
 
-export const popularEvents = pgTable("Popular Event", {
+export const venueRelations = relations(venues, ({ many }) => ({
+  eventClasses: many(eventClasses),
+  slots: many(slots),
+}));
+
+export const eventClasses = pgTable("Event Class", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }).notNull(),
+  noOfSeatsAvailable: integer("noOfSeatsAvailable").notNull(),
+  price: integer("price").notNull(),
+  venueId: integer("venueId")
+    .notNull()
+    .references(() => venues.id),
 });
 
-// export const ProfileRelations = relations(Profile, ({ many }) => ({
-//   posts: many(Post),
-// }));
+export const eventClassRelations = relations(eventClasses, ({ one }) => ({
+  venue: one(venues, {
+    fields: [eventClasses.venueId],
+    references: [venues.id],
+  }),
+}));
+
+export const slots = pgTable("Slot", {
+  id: serial("id").primaryKey(),
+  time: time("time").notNull(),
+  venueId: integer("venueId")
+    .notNull()
+    .references(() => venues.id),
+});
+
+export const slotRelations = relations(slots, ({ one }) => ({
+  venue: one(venues, {
+    fields: [slots.venueId],
+    references: [venues.id],
+  }),
+}));
